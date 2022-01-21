3df3a914f4290a4bbaa6f79c0ac9744236a7dea9
dreinasant <ao.andreina@gmail.com>
2022-01-20 23:17:52 -0300

    Desafio de logica 04

---
+++ script.js
@@ -0,0 +1,35 @@
+/*function Olá(){
+    console.log("Olá Mundo")
+}
+Olá()*/
+
+/*let nomeLindo = "Andrêina"
+function nomeExposto(nome){
+    console.log(`Esse é meu nome ${nome}`)
+}
+nomeExposto(nomeLindo)*/
+
+/*function nomeExposto(nome){
+    console.log(`Esse é meu nome ${nome}`)
+}
+nomeExposto("Andrêina")*/
+
+/*function informações(nome){
+    console.log(`Meu nome é ${nome}`)
+}
+informações("Andrêina, 21 anos, um estilo musical rap")
+
+
+function maisInformações(nome){
+    console.log(`Um filme e um dos meus estilos musicais ${nome}`)
+}
+maisInformações("365 Days, POP Smoke - DIOR")*/
+
+
+function triploDaSoma(w, z){
+    function triplo(x){
+      return 3 * x;
+    }
+    return triplo(w + z);
+  }
+  console.log(triploDaSoma(3, 2));
