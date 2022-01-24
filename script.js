4052b8fb6fb8c877e8543e7555702617f1aef9c6
dreinasant <ao.andreina@gmail.com>
2022-01-24 13:20:48 -0300

    atualização de exclusao

--- script.js
+++ script.js
@@ -21,31 +21,16 @@ nomeExposto("Andrêina")*/
 informações("Andrêina", "21 anos", "rap")*/
 
 
-function maisInformações(umFilme, umaMusica){
+/*function maisInformações(umFilme, umaMusica){
     console.log(`Um filme ${umFilme} e uma musica ${umaMusica}`)
 }
-maisInformações("365 Days", "POP Smoke - DIOR")
+maisInformações("365 Days", "POP Smoke - DIOR")*/
 
 
-/*function triploDaSoma(w, z){
+function triploDaSoma(w, z){
     function triplo(x){
       return 3 * x;
     }
     return triplo(w + z);
   }
-  console.log(triploDaSoma(3, 2))*/
-
-/*let food = "lasanha"
-    switch(food){
-    case "lasanha":
-        console.log("Não vendemos esta comida aqui")
-    break;
-    case "pizza":
-        console.log("Estamos com escassez de pizza")
-    break;
-    case "salada":
-        console.log("Aqui está, são 3 reais")
-    break;
-    default:
-        console.log("Dia invalido")
-    break;*/
+  console.log(triploDaSoma(3, 2))
