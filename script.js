4b56332aaafc5f291076aed592a58a4dbc6b675b
dreinasant <ao.andreina@gmail.com>
2022-01-24 13:14:11 -0300

    atualização

--- script.js
+++ script.js
@@ -14,22 +14,38 @@ nomeExposto(nomeLindo)*/
 }
 nomeExposto("Andrêina")*/
 
-/*function informações(nome){
-    console.log(`Meu nome é ${nome}`)
+
+/*function informações(nome, idade, estiloMusical){
+    console.log(`Meu nome é ${nome}, minha idade é ${idade} e um dos meus estilos musicais é ${estiloMusical}`)
 }
-informações("Andrêina, 21 anos, um estilo musical rap")
+informações("Andrêina", "21 anos", "rap")*/
 
 
-function maisInformações(nome){
-    console.log(`Um filme e um dos meus estilos musicais ${nome}`)
+function maisInformações(umFilme, umaMusica){
+    console.log(`Um filme ${umFilme} e uma musica ${umaMusica}`)
 }
-maisInformações("365 Days, POP Smoke - DIOR")*/
+maisInformações("365 Days", "POP Smoke - DIOR")
 
 
-function triploDaSoma(w, z){
+/*function triploDaSoma(w, z){
     function triplo(x){
       return 3 * x;
     }
     return triplo(w + z);
   }
-  console.log(triploDaSoma(3, 2));
+  console.log(triploDaSoma(3, 2))*/
+
+/*let food = "lasanha"
+    switch(food){
+    case "lasanha":
+        console.log("Não vendemos esta comida aqui")
+    break;
+    case "pizza":
+        console.log("Estamos com escassez de pizza")
+    break;
+    case "salada":
+        console.log("Aqui está, são 3 reais")
+    break;
+    default:
+        console.log("Dia invalido")
+    break;*/
